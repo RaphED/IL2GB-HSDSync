fbfacc480007b0c16295d2b13fe9ae88b1fce8bc
disdero <eric_disdero@hotmail.com>
2024-11-25 00:20:05 +0100

    back to terminal

--- main.py
+++ main.py
@@ -1,21 +1,79 @@
 import os
 import shutil
-import subprocess
-import sys
+from threading import Thread
 from tkinter import filedialog, messagebox, simpledialog
-import requests
 import tkinter as tk
 from tkinter import ttk
+import sv_ttk
 import synchronizer
 import pythonServices.configurationService as configurationService
 from pythonServices.subscriptionService import SubscribedCollection, isSubcriptionFolderEmpty, getAllSubscribedCollection
 from packaging.version import Version
-
-collectionByNameSubscribeFile=dict() #str,list[SubscribedCollection])
 from pythonServices.subscriptionService import isSubcriptionFolderEmpty, getAllSubscribedCollection
 import pythonServices.loggingService
 import logging
 from ISSupdater import checkAndPerformAutoUpdate
+
+collectionByNameSubscribeFile=dict() 
+
+def syncronize_main():
+    try:
+        performPreExecutionChecks()
+
+        #CUSTOM PHOTOS SECTION
+        cockpitNotesMode = configurationService.getConf("cockpitNotesMode")
+        if cockpitNotesMode != "noSync":
+            print(f"Custom photos scan mode : {cockpitNotesMode}")
+            print("Photos scan launched. Please wait...")
+            scanResult = synchronizer.scanCustomPhotos()
+            if len(scanResult) > 0:
+                print("Some photos has to be updated :")
+                print([customPhoto["aircraft"] for customPhoto in scanResult])
+                
+                answer = messagebox.askyesno(title='confirmation',
+                message='Do you want to perform the update ?')                    
+                if answer:
+                    print("Update started...")
+                    synchronizer.updateCustomPhotos(scanResult)
+                    print("Update done")
+                elif answer:
+                    print("ok no update")
+            else:
+                printSuccess("All custom photos are up to date")
+
+        #SKINS SECTION
+        if isSubcriptionFolderEmpty():
+            printWarning("Subscription folder is empty.\nAdd .iss file(s) to subscribe to any skins collection")
+
+        subscribedCollections = getAllSubscribedCollection()
+        print("Subscribed collections : ")
+        for collection in subscribedCollections:
+            print(f"\t-{collection.subcriptionName}")
+
+        printWarning("SKINS scan launched. Please wait...")
+        #once the prec checks passed, perform the global scan
+        scanResult = synchronizer.scanSkins()
+        printSuccess("SKINS scan finished")
+        print(scanResult.toString())
+
+        #then as the user for the update if any
+        if scanResult.IsSyncUpToDate():
+            printSuccess("All skins are up to date.")
+        else:
+            answer = messagebox.askyesno(title='confirmation',
+            message='Do you want to perform the update ?')
+            if answer:
+                printSuccess("||||||||| START SYNC ||||||||")
+                synchronizer.updateAll(scanResult)
+                printSuccess("|||||||||  END SYNC  ||||||||")
+            else:
+                print("No skin synchronization performed.")
+
+        printSuccess("I3S ended properly.")
+
+    except Exception as e:
+        printError(e)
+        printError("I3S ended with an error.")
 
 def performPreExecutionChecks():
 
@@ -82,6 +140,7 @@ def printSuccess(text):
 
     
 class MyApp:
+    g_text_widget=None
     def __init__(self, root):
         self.root = root
         self.root.title("ISS")
@@ -91,12 +150,8 @@ class MyApp:
         self.label = tk.Label(root, text="Subscriptions files .iss and .iss.disabled", font=("Arial", 10))
         self.label.pack(pady=5)  # Add some padding above the Treeview
 
-        # Set up the style for the Treeview
-        self.style = ttk.Style()
-        self.style.configure("Custom.Treeview", background="lightblue", fieldbackground="lightblue")
-
         # Create and pack the Treeview widget with the custom style
-        self.tree = ttk.Treeview(root, style="Custom.Treeview")
+        self.tree = ttk.Treeview(root, show="tree" )#, style="Custom.Treeview")
         self.tree.pack(fill="both",  padx=5, pady=5)
         
         # Add hierarchical data to the Treeview
@@ -105,7 +160,7 @@ class MyApp:
         self.tree.bind("<<TreeviewSelect>>", self.on_item_selected)
 
         # Create buttons in a frame
-        button_frame = tk.Frame(root, bg="lightgreen")
+        button_frame = tk.Frame(root)
         button_frame.pack(fill="x", pady=5)
 
         # Add background color to the button frame for visibility
@@ -115,118 +170,41 @@ class MyApp:
         self.delete_button = tk.Button(button_frame, text="Delete", command=self.delete_item)
         self.delete_button.pack(side="left", padx=5, pady=5)
 
-        self.switch_state_button = tk.Button(button_frame, text="Activer/Desactiver", command=self.switch_state)
+        self.switch_state_button = tk.Button(button_frame, text="Activate/Disable", command=self.switch_state)
         self.switch_state_button.pack(side="left", padx=5, pady=5)
         
-        self.open_terminal_button = tk.Button(button_frame, text="StartSync !", command=self.open_terminal, background="#95de97")
-        self.open_terminal_button.pack(side="left", padx=5, pady=5)
+        self.start_sync_button = tk.Button(button_frame, text="StartSync !", command=self.start_sync)
+        self.start_sync_button.pack(side="left", padx=5, pady=5)
         
 
+    def start_sync(self):
+        # TODO ERIC faire plus propre
+        # global g_text_widget
+        # """Open a new window with terminal-like functionality."""
+        # terminal = tk.Toplevel(self.root)  # Create a new Toplevel window
+        # terminal.title("Syncro des skins :")
+        # terminal.geometry("400x300")
 
+        # # Create a Text widget for displaying terminal output
+        # text_widget = tk.Text(terminal, wrap="word", height=15, width=50)
+        # text_widget.pack(expand=True, fill="both")
+        # text_widget.config(state=tk.DISABLED)  # Disable editing
+        # g_text_widget = text_widget
+        # # Simulate terminal output
+        # # self.print_to_terminal(f"Début de la syncro!")
 
+        # #The main core function :
+        Thread(target=syncronize_main(), daemon=True).start()
 
-    def open_terminal(self):
-        """Open a new window with terminal-like functionality."""
-        terminal = tk.Toplevel(self.root)  # Create a new Toplevel window
-        terminal.title("Syncro des skins :")
-        terminal.geometry("400x300")
-
-        # Create a Text widget for displaying terminal output
-        text_widget = tk.Text(terminal, wrap="word", height=15, width=50)
-        text_widget.pack(expand=True, fill="both")
-        text_widget.config(state=tk.DISABLED)  # Disable editing
-
-        # Simulate terminal output
-        self.print_to_terminal(text_widget, f"Début de la syncro!")
-
-        #The main core function :
-        try:
-            performPreExecutionChecks()
-
-            #CUSTOM PHOTOS SECTION
-            cockpitNotesMode = configurationService.getConf("cockpitNotesMode")
-            if cockpitNotesMode != "noSync":
-                self.print_to_terminal(text_widget,f"Custom photos scan mode : {cockpitNotesMode}")
-                self.print_to_terminal(text_widget,"Photos scan launched. Please wait...")
-                scanResult = synchronizer.scanCustomPhotos()
-                if len(scanResult) > 0:
-                    self.print_to_terminal(text_widget,"Some photos has to be updated :")
-                    self.print_to_terminal(text_widget,[customPhoto["aircraft"] for customPhoto in scanResult])
-                    
-                    answer = messagebox.askyesno(title='confirmation',
-                    message='Do you want to perform the update ?')                    
-                    if answer:
-                        self.print_to_terminal(text_widget,"Update started...")
-                        synchronizer.updateCustomPhotos(scanResult)
-                        self.print_to_terminal(text_widget,"Update done")
-                    elif answer:
-                        self.print_to_terminal(text_widget,"ok no update")
-                else:
-                    printSuccess("All custom photos are up to date")
-
-            #SKINS SECTION
-            if isSubcriptionFolderEmpty():
-                printWarning("Subscription folder is empty.\nAdd .iss file(s) to subscribe to any skins collection")
-
-            subscribedCollections = getAllSubscribedCollection()
-            self.print_to_terminal(text_widget,"Subscribed collections : ")
-            for collection in subscribedCollections:
-                self.print_to_terminal(text_widget,f"\t-{collection.subcriptionName}")
-
-            printWarning("SKINS scan launched. Please wait...")
-            #once the prec checks passed, perform the global scan
-            scanResult = synchronizer.scanSkins()
-            printSuccess("SKINS scan finished")
-            self.print_to_terminal(text_widget,scanResult.toString())
-            
-
-            #then as the user for the update if any
-            if scanResult.IsSyncUpToDate():
-                printSuccess("All skins are up to date.")
-            else:
-                answer = messagebox.askyesno(title='confirmation',
-                message='Do you want to perform the update ?')
-                if answer:
-                    printSuccess("||||||||| START SYNC ||||||||")
-                    synchronizer.updateAll(scanResult)
-                    printSuccess("|||||||||  END SYNC  ||||||||")
-                else:
-                    self.print_to_terminal(text_widget,"No skin synchronization performed.")
-
-            printSuccess("I3S ended properly.")
-
-        except Exception as e:
-            printError(e)
-            printError("I3S ended with an error.")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    def print_to_terminal(self, text_widget, text):
-        """Add text to the terminal output in the Text widget."""
-        text_widget.config(state=tk.NORMAL)  # Enable editing
-        text_widget.insert(tk.END, text + "\n")  # Insert text
-        text_widget.yview(tk.END)  # Auto-scroll to the end
-        text_widget.config(state=tk.DISABLED)  # Disable editing again
+    # TODO ERIC faire plus propre
+    # def print_to_terminal(self, text):
+    #     global g_text_widget
+    #     text_widget = g_text_widget
+    #     """Add text to the terminal output in the Text widget."""
+    #     text_widget.config(state=tk.NORMAL)  # Enable editing
+    #     text_widget.insert(tk.END, text + "\n")  # Insert text
+    #     text_widget.yview(tk.END)  # Auto-scroll to the end
+    #     text_widget.config(state=tk.DISABLED)  # Disable editing again
 
     def switch_state(self):
         selected_item = self.tree.selection()
@@ -286,8 +264,12 @@ class MyApp:
                     message='Are you sure you want to delete this subscription ?')
                     if answer:
                         file_name = self.tree.item(item, 'text') 
-                        file_name=file_name[:file_name.find(" Size of total:")]
-                        file_path = os.path.join("Subscriptions", file_name+'.iss')  # Path to the file in Subscriptions
+                        if file_name.find(" Size of total:")!= -1:
+                            file_name=file_name[:file_name.find(" Size of total:")]
+                            file_path = os.path.join("Subscriptions", file_name+'.iss')  # Path to the file in Subscriptions
+                        else:
+                            file_name=file_name[:file_name.find(" DISABLED")]
+                            file_path = os.path.join("Subscriptions", file_name+'.iss.disabled')  # Path to the file in Subscriptions
                         if os.path.exists(file_path):
                             os.remove(file_path)
                             for item in self.tree.get_children():
@@ -347,12 +329,9 @@ class MyApp:
             self.tree.insert("", "end", text=disabledElement)
                              
 
-   
-
-
-    
+ 
 ######### MAIN ###############
-
+app=None
 if __name__ == "__main__":
     
     checkAndPerformAutoUpdate()
@@ -360,5 +339,12 @@ if __name__ == "__main__":
     performPreExecutionChecks()
 
     root = tk.Tk()
+    
     app = MyApp(root)
+    sv_ttk.use_dark_theme()
+
     root.mainloop()
+
+
+
+
