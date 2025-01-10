c121707cbdbfa9277be2fd396400ee14590e3f2d
RaphED <raphael.escure@gmail.com>
2025-01-10 19:08:17 +0100

    Updater : remove unecessary try catch

--- ISSupdater.py
+++ ISSupdater.py
@@ -36,24 +36,21 @@ def downloadAndRunUpdater(prerelease = False):
 def replaceAndLaunchMainExe(prerelease = False):
     logging.info("Updater : Replace and Launch Main Exe")
 
-    try:
-        newExeFilePath = os.path.join(getTempFolderFullPath(), "ISS.exe")
-        #HACK : if the new exe is not there, rerun the download
-        if not os.path.exists(newExeFilePath):
-            logging.warning(f"Autoupdater Cannot find the last exe file at {newExeFilePath}")
-            return downloadAndRunUpdater(prerelease = prerelease)
-            
-        #add a timer to make sure previous main exe is stopped
-        #TODO : perform a while checker
-        time.sleep(5)
-        #copy the file next to the exe to replace with another name
-        mainExeFilePath = os.path.join(os.path.curdir, "ISS.exe")
+    newExeFilePath = os.path.join(getTempFolderFullPath(), "ISS.exe")
+    #HACK : if the new exe is not there, rerun the download
+    if not os.path.exists(newExeFilePath):
+        logging.warning(f"Autoupdater Cannot find the last exe file at {newExeFilePath}")
+        return downloadAndRunUpdater(prerelease = prerelease)
         
-        copyFile(newExeFilePath, mainExeFilePath)
+    #add a timer to make sure previous main exe is stopped
+    #TODO : perform a while checker
+    time.sleep(5)
+    #copy the file next to the exe to replace with another name
+    mainExeFilePath = os.path.join(os.path.curdir, "ISS.exe")
+    
+    copyFile(newExeFilePath, mainExeFilePath)
 
-        #then run !
-        runNewIndependantProcess([mainExeFilePath])
+    #then run !
+    runNewIndependantProcess([mainExeFilePath])
 
-    except Exception as e:
-        logging.error(e)
 
